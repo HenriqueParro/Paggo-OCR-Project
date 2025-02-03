6dd6e8f0ca2df1938db6088c575e3742809a626f
Henrique Parro <59770362+HenriqueParro@users.noreply.github.com>
2025-02-03 11:45:40 -0300

    Update auth.service.ts

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'prisma/prisma.service';
-//import { PrismaService } from '../prisma/prisma.service';
+//import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 
 @Injectable()
