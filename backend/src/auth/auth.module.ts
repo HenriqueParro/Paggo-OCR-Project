90a2488a824e3c45191d99373f66f93a309a4256
Henrique Parro <59770362+HenriqueParro@users.noreply.github.com>
2025-02-03 11:37:26 -0300

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from 'src/jwt.strategy';
-//import { PrismaModule } from 'prisma/primas.modules'; 
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaModule } from 'prisma/primas.modules'; 
+//import { PrismaService } from '../../prisma/prisma.service';
 
 
 @Module({
