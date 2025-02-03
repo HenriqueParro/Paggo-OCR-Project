dd5ae5d87cb9ca79fdb1351bdaac25eeb70c7fbc
Henrique Parro <59770362+HenriqueParro@users.noreply.github.com>
2025-02-03 11:45:21 -0300

    Delete backend/dist directory

--- backend/dist/src/ocr/ocr.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class OcrService {
-    extractText(filePath: string): Promise<string>;
-}
--- backend/dist/src/upload/upload.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaService } from 'prisma/prisma.service';
-import { Document } from '@prisma/client';
-import { OcrService } from '../ocr/ocr.service';
-export declare class UploadService {
-    private readonly prisma;
-    private readonly ocrService;
-    constructor(prisma: PrismaService, ocrService: OcrService);
-    saveDocument(userId: number, fileName: string, filePath: string): Promise<Document>;
-}
